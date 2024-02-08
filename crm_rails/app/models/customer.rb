393724b6ca42567e9a40d5744b443ae4753e32ae
Jerinkc <jerinkc22@gmail.com>
2024-02-08 16:21:47 +0530

    Email unique validation

--- crm_rails/app/models/customer.rb
+++ crm_rails/app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < User
   has_one :contact
 
   validates :email, :full_name, :contact, presence: true
-
+  validates :email, uniqueness: true
 
   accepts_nested_attributes_for :contact
 end
